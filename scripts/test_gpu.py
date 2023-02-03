45aa7ea0323ca6bf5738ba05af00500bb605e978
timothyas <timsmith204@utexas.edu>
2023-02-03 16:49:43 -0700

    gpu test updated

--- scripts/test_gpu.py
+++ scripts/test_gpu.py
@@ -1,71 +1,122 @@
+import os
 import cupy as cp
 import numpy as np
 import matplotlib.pyplot as plt
+import logging
+from contextlib import redirect_stdout
 
 from ddc import DataLorenz96
+
+from timer import Timer
 
 import sys
 sys.path.append("..")
 from esnpy import ESN, from_zarr
 
+class ScalingTest():
 
-def generate_data(
-        system_dimension=6,
-        n_train=42_000,
-        n_test=1_001,
-        n_spinup=500):
+    n_system    = None
+    n_reservoir = None
+    n_train     = 100_000
+    n_test      = 2_000
+    n_spinup    = 500
+    batch_size  = None
+    out_dir     = "scaling-results"
+
+    def __init__(self, **kw):
+        for key, val in kw.items():
+            try:
+                getattr(self, key)
+            except:
+                raise
+            setattr(self, key, val)
 
 
-    data = DataLorenz96(system_dimension=system_dimension)
-    data.generate(n_steps=n_train)
-    trainer = data.values.copy()
+        # setup logging
+        os.makedirs(self.out_dir)
+        self.logfile = os.path.join(self.out_dir, "stdout.log")
 
-    data.generate(n_steps=1_000)
-    data.generate(n_steps=n_test+n_spinup)
-    tester = data.values.copy()
-    return cp.asarray(trainer), cp.asarray(tester)
+        # timers
+        self.walltime = Timer(filename=self.logfile)
+        self.localtime = Timer(filename=self.logfile)
+
+
+    def __call__(self):
+
+        self.walltime.start()
+        self.localtime.start("Generate Data")
+        trainer, tester = self.generate_data()
+        self.localtime.stop()
+
+        self.localtime.start("Create ESN")
+        esn = self.get_esn()
+        self.localtime.stop()
+
+        self.localtime.start("Training")
+        esn.train(trainer, batch_size=self.batch_size)
+        self.localtime.stop("Training time")
+
+        self.localtime.start("Prediction")
+        y = esn.predict(tester, n_steps=self.n_test, n_spinup=self.n_spinup)
+        self.localtime.stop("Prediction time")
+
+        self.localtime.start("Store model")
+        ds = esn.to_xds()
+        ds.to_zarr(os.path.join(self.out_dir, "esn.zarr"))
+        self.localtime.stop("IO time")
+
+        self.walltime.stop("Total Walltime")
+
+
+    def print_log(self, *args):
+        with open(self.logfile, "a") as file:
+            with redirect_stdout(file):
+                print(*args)
+
+
+    def generate_data(self):
+
+        data = DataLorenz96(system_dimension=self.n_system)
+        data.generate(n_steps=self.n_train)
+        trainer = data.values.copy()
+
+        data.generate(n_steps=1_000)
+        data.generate(n_steps=self.n_test+self.n_spinup+1)
+        tester = data.values.copy()
+        return cp.asarray(trainer), cp.asarray(tester)
+
+
+    def get_esn(self):
+
+        esn = ESN(
+            n_input=self.n_system,
+            n_output=self.n_system,
+            n_reservoir=self.n_reservoir,
+            input_factor=0.86,
+            adjacency_factor=0.71,
+            connectedness=5,
+            bias=1.76,
+            leak_rate=0.87,
+            tikhonov_parameter=6.9e-7,
+            input_kwargs={
+                "normalization": "svd",
+            },
+            adjacency_kwargs={
+                "normalization": "svd",
+            },
+            random_seed=0,
+        )
+        esn.build()
+        return esn
 
 
 if __name__ == "__main__":
 
-    system_dimension = 6
 
-    esn = ESN(
-        n_input=system_dimension,
-        n_output=system_dimension,
-        n_reservoir=500,
-        input_factor=0.863,
-        adjacency_factor=0.713,
-        connectedness=5,
-        bias=1.76,
-        leak_rate=0.874,
-        tikhonov_parameter=6.9e-7,
-        input_kwargs={
-            "normalization": "svd",
-        },
-        adjacency_kwargs={
-            "normalization": "svd",
-        },
-        random_seed=0,
-    )
-
-    trainer, tester = generate_data(system_dimension=system_dimension)
-
-    esn.build()
-    esn.train(trainer)
-    y = esn.predict(tester, n_steps=1_000, n_spinup=500)
-    u = tester[:, 500:]
-
-    time = 0.01 * np.arange(y.shape[-1])
-
-
-    nrows = system_dimension
-    fig, axs = plt.subplots(nrows, 1, figsize=(8, nrows*1), constrained_layout=True, sharex=True, sharey=True)
-
-    for ui, yi, ax in zip(u.get(), y.get(), axs):
-        ax.plot(time, ui, color='k')
-        ax.plot(time, yi, color="C2")
-        for key in ["top", "right"]:
-            ax.spines[key].set_visible(False)
-
-    fig.savefig("esn-prediction.pdf", bbox_inches="tight")
+    n_system = 32
+    for n_reservoir in [500, 1000, 2000, 4000, 8000]:
+        test = ScalingTest(
+                n_system=n_system,
+                n_reservoir=n_reservoir,
+                out_dir=f"psl-gpu-test/{n_reservoir:06d}nr-{n_system:03d}ns")
+        test()
