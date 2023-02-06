6e08d3f0b42981a32f0f8c18343684b4a7efe25c
timothyas <smith.timothy.andrew@gmail.com>
2023-02-06 22:07:42 +0000

    gcp timinig

--- scripts/test_on_gcp.py
+++ scripts/test_on_gcp.py
@@ -11,12 +11,21 @@ from test_gpu import ScalingTest
 if __name__ == "__main__":
 
 
-    n_system = 8
-
     pstr = "gpu" if _use_cupy else "cpu"
-    for n_reservoir in [500, 1000, 2000, 4000, 8000, 16000]:
+    n_system = 128
+
+    batches = {
+            500     : 100_000,
+            1_000   : 100_000,
+            2_000   :  50_000,
+            4_000   :  25_000,
+            8_000   :  12_500,
+            16_000  :   6_250}
+
+    for n_reservoir in [500,   1_000,  2_000,  4_000,  8_000, 16_000]:
         test = ScalingTest(
                 n_system=n_system,
                 n_reservoir=n_reservoir,
+                batch_size=batches[n_reservoir],
                 out_dir=f"gcp-{pstr}-test/{n_reservoir:06d}nr-{n_system:03d}ns")
         test()
