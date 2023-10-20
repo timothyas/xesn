a13c6cc35d34d49a3bb6691a6e951bff9ea7bce5
timothyas <timsmith204@utexas.edu>
2023-10-20 16:40:33 -0600

    a basic psd module

---
+++ xesn/psd.py
@@ -0,0 +1,114 @@
+import numpy as np
+from scipy.stats import binned_statistic
+from scipy.fft import fft2
+
+def psd_1d(xda):
+    """Compute the 1D Power Spectral Density of a 1D array, varying in time.
+
+    Note:
+        This function is not lazy, and will call the entire array into memory before computing the PSD.
+
+    Args:
+        xda (xarray.DataArray): with "time" as final dimension
+
+    Returns:
+        xda_hat (xarray.DataArray): with 1D PSD along new, nondimensional 'k1d' dimension
+    """
+
+    assert xda.dims(-1) == "time", "psd_1d requires 'time' to be on the last dimension"
+
+    n_x = xda.shape[0]
+    n_time = len(xda["time"])
+
+    # transform and get amplitudes
+    f_hat = fft(xda.values.T)
+    psd = np.abs(f_hat)**2
+
+    # get frequencies
+    n_k = n_x//2 + 1
+    k = n_x * fftfreq(n_x)
+
+    # go 1D
+    k_bins = np.arange(.5, n_k, 1.)
+    k_vals = 0.5 * (k_bins[1:] + k_bins[:-1])
+
+    psd_1d = np.zeros((n_x//2, n_time))
+    for n in range(n_time):
+        tmp1d, *_ = binned_statistic(
+            k,
+            psd,
+            statistic="mean",
+            bins=k_bins,
+        )
+        psd_1d[...,n] = tmp1d * np.pi * (k_bins[1:]**2 - k_bins[:-1]**2)
+    xda_hat = _xpack(xda, k_vals, psd_1d)
+    return xda_hat
+
+
+def psd_2d(xda):
+    """Compute the 1D Power Spectral Density of a 2D or 3D array, varying in time. If 3D, then average over 3rd spatial dimension (e.g., depth).
+
+    Note:
+        This function is not lazy, and will call the entire array into memory before computing the PSD.
+
+    Args:
+        xda (xarray.DataArray): with "time" as final dimension
+
+    Returns:
+        xda_hat (xarray.DataArray): with 1D PSD along new, nondimensional 'k1d' dimension
+    """
+
+    assert xda.dims(-1) == "time", "psd_2d requires 'time' to be on the last dimension"
+
+    n_x = xda.shape[0]
+    n_time = len(xda["time"])
+
+    # transform and get amplitudes
+    f_hat = fft2(xda.values.T)
+    psd = np.abs(f_hat)**2
+
+    # get frequencies
+    n_k = n_x//2 + 1
+    k = n_x * fftfreq(n_x)
+    k2d = np.meshgrid(k, k)
+    k_tot = np.sqrt(k2d[0]**2 + k2d[1]**2)
+
+    # go 1D
+    k_bins = np.arange(.5, n_k, 1.)
+    k_vals = 0.5 * (k_bins[1:] + k_bins[:-1])
+
+    psd_1d = np.zeros((n_x//2, n_time))
+    for n in range(n_time):
+        arr2d = psd[n,...].mean(axis=0) if psd.ndim>3 else psd[n]
+        tmp1d, *_ = binned_statistic(
+            k_tot.flatten(),
+            arr2d.flatten(),
+            statistic="mean",
+            bins=k_bins,
+        )
+        psd_1d[...,n] = tmp1d * np.pi * (k_bins[1:]**2 - k_bins[:-1]**2)
+
+
+    xda_hat = _xpack(xda, k_vals, psd_1d)
+    return xda_hat
+
+def _xpack(xda, k_vals, psd_1d):
+
+
+    attrs = {}
+    if "long_name" in xda.attrs:
+        attrs["long_name"] = f"psd_of_"+xda.attrs["long_name"]}
+    xda_hat = xr.DataArray(
+            psd_1d,
+            coords={
+                "k1d": k_vals,
+                "time": xda["time"],
+                },
+            dims=("k1d", "time"),
+            attrs=attrs,
+        }
+    xda_hat["k1d"].attrs={
+            "units": "",
+            "description": "nondimensional 1D wavenumber",
+        }
+    return xda_hat
